6d475ec0200c3fae33ad531a7da5a8183e7e493e
Carlos Sergio de Paiva Araujo <carlossdparaujo@gmail.com>
2016-08-12 13:28:34 -0300

    Criando lógica para montar o labirinto do pacman.

--- MultiPacMan/Assets/Scripts/GameController.cs
+++ MultiPacMan/Assets/Scripts/GameController.cs
@@ -3,6 +3,37 @@ using System.Collections;
 
 public class GameController : Photon.PunBehaviour {
 
+	[SerializeField]
+	private GameObject wallPrefab;
+
+	private int[,] maze = new int[25, 23] {
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+		{ 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0 },
+		{ 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0 },
+		{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+		{ 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0 },
+		{ 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0 },
+		{ 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 1, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 1, 0, 0, 0, 0, 0 },
+		{ 2, 2, 2, 2, 2, 1, 2, 2, 0, 1, 1, 2, 1, 1, 0, 2, 2, 1, 2, 2, 2, 2, 2 },
+		{ 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 1, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 1, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1, 0, 0, 0, 0, 0 },
+		{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+		{ 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0 },
+		{ 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0 },
+		{ 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0 },
+		{ 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0 },
+		{ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+		{ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+		{ 0, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+	};
+
 	void Start() {
 		PhotonNetwork.ConnectUsingSettings("0.0.0");
 	}
@@ -13,17 +44,32 @@ public class GameController : Photon.PunBehaviour {
 
 	public override void OnJoinedRoom() {
 		base.OnJoinedRoom();
-		CreatePellet();
-		CreatePlayer();
+
+		CreateMaze();
 	}
 
-	private void CreatePellet() {
-		if (PhotonNetwork.isMasterClient) {
-			PhotonNetwork.InstantiateSceneObject("Pellet", new Vector3(1.0f, 1.0f, 0.0f), Quaternion.identity, 0, null);
+	private void CreateMaze() {
+		Vector2 position = Vector2.zero;
+
+		for (int i = 0; i < maze.GetLength(0); ++i) {
+			for (int j = 0; j < maze.GetLength(1); ++j) {
+				int obj = maze[i, j];
+
+				if (obj == 0) {
+					Instantiate(wallPrefab, position, Quaternion.identity);
+				} else if (obj == 1) {
+					if (PhotonNetwork.isMasterClient) {
+						PhotonNetwork.InstantiateSceneObject("Pellet", new Vector3(position.x, position.y, 0.0f), Quaternion.identity, 0, null);
+					}
+				} else if (obj == 5) {
+					PhotonNetwork.Instantiate("Player", position, Quaternion.identity, 0);
+				}
+
+				position.x += wallPrefab.GetComponent<SpriteRenderer>().sprite.bounds.extents.x * 2;
+			}
+
+			position.x = 0.0f;
+			position.y += wallPrefab.GetComponent<SpriteRenderer>().sprite.bounds.extents.y * 2;
 		}
-	}
-
-	private void CreatePlayer() {
-		PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity, 0);
 	}
 }
