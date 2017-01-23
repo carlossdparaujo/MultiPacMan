19410a2328a463b0ae784b59f5d871fe4bba040e
Luan Martins <lawncm@gmail.com>
2017-01-23 21:53:20 -0200

    Fixed UI Scales

---
+++ MultiPacMan/Assets/Scripts/Player/TurboController/NetworkedTurboController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MultiPacMan.Player.Turbo
+{
+	public class NetworkedTurboController : TurboController {
+		public delegate bool IsTurboInUse();
+		public IsTurboInUse turboDelegate;
+
+		private bool isTurboOn = false;
+
+		public override bool IsTurboOn() {
+			return isTurboOn;
+		}
+
+		void Update() {
+			if (turboDelegate == null) {
+				return;
+			}
+
+			if (turboDelegate()) {
+				
+			} else {
+				
+			}
+
+			isTurboOn = turboDelegate ();
+		}
+	}
+}
