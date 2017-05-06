370304f48ec576756cde3517165d03b885487425
Luan Martins <lawncm@gmail.com>
2017-05-06 14:58:54 -0300

    Adicionando mostrador de Ping na tela

---
+++ MultiPacMan/Assets/PingDisplay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PingDisplay : MonoBehaviour {
+	public Text pingText;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pingText.text = "Ping: " + PhotonNetwork.networkingPeer.RoundTripTime;
+	}
+}
