5e89fb5020d157dc80874de732d892b37be2934e
caraujo <caraujo@thoughtworks.com>
2018-03-03 08:17:08 -0300

    Hide options in Advanced Menu

---
+++ MultiPacMan/Assets/AdvancedMenuButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdvancedMenuButton : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject menu;
+
+	public void ToggleMenu() {
+		menu.SetActive(!menu.activeSelf);
+	}
+}
