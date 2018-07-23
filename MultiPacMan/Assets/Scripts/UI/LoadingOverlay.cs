fe4f73d947f9831228253dc1d82c5e1ac73f93b6
Carlos Araujo <caraujo@thoughtworks.com>
2018-07-23 02:36:34 -0300

    Added loading overlay

---
+++ MultiPacMan/Assets/Scripts/UI/LoadingOverlay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using MultiPacMan.Player;
+using MultiPacMan.Game;
+
+namespace MultiPacMan.UI {
+    public class LoadingOverlay : MonoBehaviour {
+        void Start () {
+            GameController.playersStatsDelegate += DeactivateOnMyPlayerLoad;
+        }
+
+		void OnDestroy () {
+            GameController.playersStatsDelegate -= DeactivateOnMyPlayerLoad;
+        }
+
+		void DeactivateOnMyPlayerLoad (PlayersStats playersStats) {
+			if (playersStats.MyPlayerStats != null) {
+				this.gameObject.SetActive (false);
+			}
+		}
+    }
+}
